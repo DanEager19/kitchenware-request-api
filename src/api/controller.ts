81c4d5b046c3812ee715f48aa03581bc5123ef7a
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-14 22:24:42 -0500

    fleshed out queries

--- src/api/controller.ts
+++ src/api/controller.ts
@@ -1,4 +1,4 @@
-import {Item, ReservationRequest, createReservationTable, createItemTable} from './model';
+import {Item, ReservationRequest, createReservationTable, createItemTable, ItemRequest} from './model';
 import { Response } from 'express';
 const { Client, Pool } = require('pg');
 
@@ -16,10 +16,17 @@ export class Controller {
         const data = req.body;
         const item: Item = await this.client.query('SELECT * FROM items WHERE name=$1', data.item);
         if (item.isReserved) {
+            //should use the item type
             res.send(`Cannot Reserve. ${data.reservee} has ${data.item} until ${data.endDate}.`);
         } else {
-            item.isReserved = true;
-            await this.client.query('INSERT INTO reservations VALUES($1, $2, $3, $4, $5)', [
+            await this.client.query('UPDATE items SET isReserved=true, holderID=$1 WHERE holderID=$1;', [data.id, data.id])
+            await this.client.query(`INSERT INTO reservations(
+                    item, 
+                    reservee, 
+                    startDate, 
+                    endDate, 
+                    returned
+                ) VALUES($1, $2, $3, $4, $5);`, [
                 data.item,
                 data.reservee,
                 data.startDate,
@@ -31,27 +38,30 @@ export class Controller {
     }
 
     cancel = async (req: ReservationRequest, res: Response) =>{ 
-
+        const data = req.body;
+        const item: Item = await this.client.query('SELECT * FROM items WHERE name=$1;', [data.item]);
+        await this.client.query('DELETE FROM reservations WHERE ID=$1;', [data.id]);
+        await this.client.query('UPDATE items SET isReserved=false, holderID=$1 WHERE holderID=$2;', [data.id, 0])
     }
 
     showAllReservations = async (req: ReservationRequest, res: Response) =>{
 
     }
 
-    addItem = async (req: ReservationRequest, res: Response) => {
+    addItem = async (req: ItemRequest, res: Response) => {
 
     }
 
-    listAllItems = async (req: ReservationRequest, res: Response) => {
+    listAllItems = async (req: ItemRequest, res: Response) => {
         const { items } = await this.client.query('SELECT * FROM items');
         res.json(items);
     }
 
-    updateItem = async (req: ReservationRequest, res: Response) => {
+    updateItem = async (req: ItemRequest, res: Response) => {
 
     }
 
-    removeItem= async (req: ReservationRequest, res: Response) => {
+    removeItem = async (req: ItemRequest, res: Response) => {
 
     }
 }
